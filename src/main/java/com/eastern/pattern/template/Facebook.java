c567d0fe9d7afe57a24a2104b0e441e35d701d1c
EasternMoon <cs18570643214@163.com>
2022-09-20 15:52:20 +0800

    template

---
+++ src/main/java/com/eastern/pattern/template/Facebook.java
@@ -0,0 +1,58 @@
+package com.eastern.pattern.template;
+
+/**
+ * @Author chensheng13
+ * @Description TODO
+ * @Date 2022/9/20 15:40
+ * @Version 1.0
+ */
+public class Facebook extends Network {
+    public Facebook(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+
+    @Override
+    boolean logIn(String userName, String password) {
+        System.out.println("\nChecking user's parameters");
+        System.out.println("Name: " + this.userName);
+        System.out.print("Password: ");
+        for (int i = 0; i < this.password.length(); i++) {
+            System.out.print("*");
+        }
+        simulateNetworkLatency();
+        System.out.println("\n\nLogIn success on Facebook");
+        return true;
+    }
+
+    @Override
+    boolean sendData(byte[] data) {
+        boolean messagePosted = true;
+        if (messagePosted) {
+            System.out.println("Message: '" + new String(data) + "' was posted on Facebook");
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    void logOut() {
+        System.out.println("User: '" + userName + "' was logged out from Facebook");
+    }
+
+    private void simulateNetworkLatency() {
+        try {
+            int i = 0;
+            System.out.println();
+            while (i < 10) {
+                System.out.print(".");
+                Thread.sleep(500);
+                i++;
+            }
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/eastern/pattern/template/Network.java
@@ -0,0 +1,41 @@
+package com.eastern.pattern.template;
+
+/**
+ * @Author chensheng13
+ * @Description TODO
+ * @Date 2022/9/20 15:39
+ * @Version 1.0
+ */
+
+
+/**
+ * Base class of social network.
+ */
+public abstract class Network {
+    String userName;
+    String password;
+
+    Network() {
+    }
+
+    /**
+     * Publish the data to whatever network.
+     */
+    public boolean post(String message) {
+        // Authenticate before posting. Every network uses a different
+        // authentication method.
+        if (logIn(this.userName, this.password)) {
+            // Send the post data.
+            boolean result = sendData(message.getBytes());
+            logOut();
+            return result;
+        }
+        return false;
+    }
+
+    abstract boolean logIn(String userName, String password);
+
+    abstract boolean sendData(byte[] data);
+
+    abstract void logOut();
+}
